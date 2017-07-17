a69fe89708d68be5126674c2e39b8c275343d6eb
Mitchell van Wijngaarden <mitchell@kooding.nl>
2017-07-17 16:35:02 +0200

    remove files

--- src/ComponentBootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php namespace Monolith\DependencyInjection;
-
-interface ComponentBootstrap {
-    public function bind(Container $container): void;
-    public function start(Container $container): void;
-}
