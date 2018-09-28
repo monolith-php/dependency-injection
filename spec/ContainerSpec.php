3af20c75ca92eee117bb5ec676e42b8061115c3d
Shawn McCool <shawn@heybigname.com>
2018-09-28 10:52:18 +0200

    stuff

--- spec/ContainerSpec.php
+++ spec/ContainerSpec.php
@@ -3,29 +3,35 @@
 use Monolith\DependencyInjection\Container;
 use PhpSpec\ObjectBehavior;
 
-class ContainerSpec extends ObjectBehavior {
+class ContainerSpec extends ObjectBehavior
+{
 
-    function it_is_initializable() {
+    function it_is_initializable()
+    {
         $this->shouldHaveType(Container::class);
     }
 
-    function it_can_resolve_simple_dependencies_even_when_unbound() {
+    function it_can_resolve_simple_dependencies_even_when_unbound()
+    {
         $this->get(SimpleDependency::class)->shouldHaveType(SimpleDependency::class);
     }
 
-    function it_can_bind_resolution_functions_to_names() {
-        $this->bind(ComplexDependency::class, function() {
+    function it_can_bind_resolution_functions_to_names()
+    {
+        $this->bind(ComplexDependency::class, function () {
             return new ComplexDependency(new SimpleDependency, new SimpleDependency);
         });
         $this->get(ComplexDependency::class)->shouldHaveType(ComplexDependency::class);
     }
 
-    function it_can_bind_a_resolution_target_to_names() {
+    function it_can_bind_a_resolution_target_to_names()
+    {
         $this->bind(SimpleDependencyAbstraction::class, SimpleDependency::class);
         $this->get(SimpleDependencyAbstraction::class)->shouldHaveType(SimpleDependency::class);
     }
 
-    function it_can_resolve_new_instances_on_resolution() {
+    function it_can_resolve_new_instances_on_resolution()
+    {
         $this->bind(SimpleDependency::class, SimpleDependency::class);
 
         $dependency1 = $this->get(SimpleDependency::class);
@@ -34,7 +40,8 @@ class ContainerSpec extends ObjectBehavior {
         $dependency1->shouldNotBe($dependency2);
     }
 
-    function it_can_resolve_a_singleton_instance() {
+    function it_can_resolve_a_singleton_instance()
+    {
         $this->singleton(SimpleDependency::class);
 
         $dependency1 = $this->get(SimpleDependency::class);
